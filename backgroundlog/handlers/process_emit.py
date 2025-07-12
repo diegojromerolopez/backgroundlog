a191310fc23abd973216b10446695a49bad9ffeb
diegojromerolopez <diegojromerolopez@gmail.com>
2025-07-12 19:37:13 +0200

    fix: Remove dead code & update CHANGELOG

--- backgroundlog/handlers/process_emit.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-
-def process_emit(handler_class, handler_config, record_dict):
-    handler = handler_class(**handler_config)
-    record = logging.makeLogRecord(record_dict)
-    handler.emit(record)
-    handler.close()
